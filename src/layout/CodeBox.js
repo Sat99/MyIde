6ac87c7857c4affc0f72e9cfcb1e068bf25b84e4
Satwik Bhardwaj <sat12bhardwaj@gmail.com>
2019-07-23 21:50:12 +0530

    responsive

--- src/layout/CodeBox.js
+++ src/layout/CodeBox.js
@@ -107,7 +107,7 @@ const CodeBox = () => {
   const onChange2 = e => setInput(e.target.value);
   return (
     <Fragment>
-      <nav className="navbar bg-info text-white m-0" style={{ height: "100%" }}>
+      <nav className="navbar bg-info text-white m-2" style={{ height: "80%" }}>
         <ul>
           <li>
             <button
